265afc216f544c4b2e75c60b78b379ac9f0f6d31
fadiamrani <fadi.amrani@gmail.com>
2024-02-23 17:35:49 +0100

    Add files via upload

---
+++ pingz.rs
@@ -0,0 +1,37 @@
+extern crate ping;
+extern crate rand;
+
+use std::time::Duration;
+
+use rand::random;
+
+#[test]
+pub fn basic() {
+    let addr = "127.0.0.1".parse().unwrap();
+    let timeout = Duration::from_secs(1);
+    ping::ping(
+        addr,
+        Some(timeout),
+        Some(166),
+        Some(3),
+        Some(5),
+        Some(&random()),
+    )
+        .unwrap();
+    //Ok(Some(format!("Hello")))
+}
+
+#[test]
+pub fn basic_v6() {
+    let addr = "::1".parse().unwrap();
+    let timeout = Duration::from_secs(1);
+    ping::ping(
+        addr,
+        Some(timeout),
+        Some(166),
+        Some(3),
+        Some(5),
+        Some(&random()),
+    )
+        .unwrap();
+}
